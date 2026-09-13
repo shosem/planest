ce6952368c8d4ec12bfaf689708597d4315b068b
shosem <shousei4794@icloud.com>
2026-09-13 23:20:53 +0900

    fix: lintエラーの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-        registrations: 'users/registrations'
+        registrations: "users/registrations"
       }
   get "pages/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
