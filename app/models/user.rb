85e02f9a4ceac8a9d730279a4f46f5b5ea84ac42
shosem <shousei4794@icloud.com>
2026-09-14 01:09:27 +0900

    Add: バリデーションの追加
    nameは12文字で設定しているが、今後UIとの兼ね合いで様子を見る

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
+  validates :name, presence: true, length: { minimum: 12 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
 end
