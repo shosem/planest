e66dd18f60b1541d0b4a3948fc6b2d26532a5ae6
shosem <shousei4794@icloud.com>
2026-09-14 15:01:48 +0900

    Feat: 認証機能の追加 (#15)
    * Add: gem deviseの追加
    
    deviseのインストールも済んでいる。
    同時にdeviseの日本語化用gemもインストール。
    
    * Add: devise用の日本語ファイルを追加
    
    * chore: アプリの言語設定を日本に
    
    * Add: Userモデルの作成
    
    nameカラムを追加しマイグレート。
    パスワードリセットは今回実装しないため削除した。
    
    * wip: Devise導入作業中
    
    * feat: 認証画面の作成
    
    読み込み時間も少なく、JSも不要なためタブ切り替えはページ遷移で実装した。
    デモではタブのroleをtabと設定していたが、実際はリンクの羅列でありタブではないため削除した。
    現在見ているページはaria-current="page"にて判定。
    
    * fix: lintエラーの修正
    
    * remove: 認証にまつわるリンクパーシャルを削除
    
    デフォルトでは使用していたが、今回のアプリでは使用しないため削除した。
    
    * Add: バリデーションの追加
    
    nameは12文字で設定しているが、今後UIとの兼ね合いで様子を見る

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_10_014759) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_12_143143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 end
