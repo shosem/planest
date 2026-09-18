db9c89f9b56ddf683a22acd21b03cce1cf38e761
kan_chan <shunplay1793@gmail.com>
2026-09-18 20:53:58 +0900

    refactor/paramsでグループのレコードを取得し、ivarで管理するように修正 refactor/view側で@groupを参照して、グループ名・招待コードを表示するように修正（存在チェック時もivarを参照） アクセス権限のないグループを参照しようとした時の404エラー挙動については、 まだ試行錯誤の段階のため途中経過

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,12 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
   def show
-    
+    @group = Group.find(params[:id])
+
+    # 自分がホストのグループ以外にアクセスしようとすると個人ページへ戻される（本当は参加中のグループかどうかを判定したい）
+    unless @group.owner_id == current_user.id
+      # ログインユーザーのpersonalグループのidを取得している（本当は404エラー画面へ行きたい）
+      redirect_to group_path(current_user.personal_group)
+    end
   end
 end
