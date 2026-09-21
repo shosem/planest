9fbaad2b59642112456350778b4e6fff1663b7be
shosem <shousei4794@icloud.com>
2026-09-21 12:05:51 +0900

    feat: グループ作成機能 (#35)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,27 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    # user.groups.buildの場合、current_user.groupsにbuildしたオブジェクトが反映されてしまう
+    @group.owner = current_user
+
+    @group.group_members.build(user: current_user)
+    if @group.save
+      redirect_to group_path(@group), success: "グループを作成しました"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
   def show; end
+
+  private
+
+  def group_params
+    params.expect(group: [ :name ])
+  end
 end
