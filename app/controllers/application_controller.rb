1ac2149781fa56febc99accd9f6cec6918481064
kan_chan <shunplay1793@gmail.com>
2026-09-22 00:00:56 +0900

    feature/グループ詳細画面の実装 (#27)
    * add/グループ詳細画面でグループ名の取得
    
    * change/インスタンス変数の取得を削除（グループ情報の取得はApplication_controllerのヘルパーメソッドで良さそう
    
    * add/グループ詳細画面での招待コードの取得・表示
    
    * add/ログイン後のページ遷移先を個人グループに指定（application_controller:21 ~ 26）
    add/新規登録時にグループが作成されていなかった場合のエラー回避のためのif分を追加（groups/show.html.erb:2）
    
    <% if current_group.present? %>を追記しないと、「current_group.nameが見つからない」エラーが出るため、
    おそらく「グループが」
    作成されていないままcurrent_groupを参照してしまうと、データがないためエラーが出ると思われます。
    
    * add/招待コードの存在チェックを追加（なくても動いたけど安全性のため）
    
    * add/css調整
    
    * 修正箇所の参照先コメントを追加
    
    * refactor/paramsでグループのレコードを取得し、ivarで管理するように修正
    refactor/view側で@groupを参照して、グループ名・招待コードを表示するように修正（存在チェック時もivarを参照）
    アクセス権限のないグループを参照しようとした時の404エラー挙動については、
    まだ試行錯誤の段階のため途中経過
    
    * add/アクセス権限がないグループへの詳細遷移を弾く処理を追記
    
    * pages#home復活
    
    * ルートページへアクセスしたときに、ログインユーザーの個人ページへ遷移させる
    
    * fix/存在しないグループIDへアクセスしようとした時の例外エラー対策
    params[:id]Groupオブジェクト取得時にfindを使っていた箇所を、find_byに変更してnilを返すように変更
    
    これにより、後続のunless文に正常に入れるため、存在しないページへ参加しようとした場合も、挙動を行うようになった
    参加していないグループIDへアクセスしようとした時同様に、弾かれる挙動を行うようになった。
    
    * fix/グループ詳細viewのレイアウトをひーちゃんレイアウトに修正
    一旦グループ名だけ動的に取得 → 表示するようにした
    タスク数/タスクの一覧表示はまだ
    
    * fix/specファイルの修正
    ルートページにリダイレクトするのに合わせて、 success → 302 検知に変更
    
    * fix/lint エラーの修正
    
    * コード整形
    
    * fix/参照するHTTPステータスコードを番号直書きからシンボルに変更
    
    * fix/mainタグの重複を修正
    show.html.erbの外側を囲んでいたmainタグをdivに修正
    
    * fix/タスク追加ボタンの「＋」アイコンの読み込み処理をパーシャルファイルに切り分け
    
    * プラスアイコンのサイズ3に合わせる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,10 @@ class ApplicationController < ActionController::Base
     id = params[:group_id] || params[:id]
     @current_group = id && current_user.groups.detect { |group| group.id == id.to_i }
   end
+
+  # ログイン後のページ遷移先を自分の個人グループに固定
+  def after_sign_in_path_for(resource)
+    # 遷移先のパス（個人用グループのidを参照する）
+    group_path(resource.personal_group)
+  end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,7 +17,16 @@ class GroupsController < ApplicationController
       render :new, status: :unprocessable_content
     end
   end
-  def show; end
+
+  def show
+    @group = Group.find_by(id: params[:id])
+
+    # 自分が参加中のグループ以外にアクセスしようとすると弾かれる
+    unless current_user.groups.include?(@group)
+      # ログインユーザーのpersonalグループのidを取得している（本当は404エラー画面へ行きたい）
+      redirect_to group_path(current_user.personal_group)
+    end
+  end
 
   private
 
