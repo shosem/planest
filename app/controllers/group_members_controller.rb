582fa73844e4e95afffe446a7e6d4512f4d81897
shosem <shousei4794@icloud.com>
2026-09-21 14:25:20 +0900

    feat: グループ招待機能
    参加済みのグループの招待画面に遷移したときは、そのグループの詳細画面に遷移するようにした。

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -1,8 +1,14 @@
 class GroupMembersController < ApplicationController
   def new
+    @group = Group.find_by(invite_code: params[:invite_code])
+    if current_user.groups.include?(@group)
+      redirect_to group_path(@group), info: "すでに参加しています"
+    end
   end
 
   def create
-    
+    @group = Group.find_by(invite_code: params[:invite_code])
+    @group.group_members.create!(user: current_user)
+    redirect_to group_path(@group), success: "#{@group.name}に参加しました"
   end
 end
