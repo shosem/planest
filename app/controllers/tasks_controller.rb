1ade5087dc95b5290ef2ad7827f5666e32a869a1
hee1120 <fijuni1120@gmail.com>
2026-09-26 01:53:07 +0900

    fix: lintエラー対応

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,19 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
   before_action :set_group
-  
+
   def create
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def show
   end
-  
+
   def destroy
   end
 
