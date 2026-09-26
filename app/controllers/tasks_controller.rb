e9914a5c9cd66ea16e4eed8d44fd25be1c29d61f
hee1120 <78842377+hee1120@users.noreply.github.com>
2026-09-26 20:01:44 +0900

    Feat: タスク詳細画面の静的ファイルを実装 (#50)
    * feat: デモからスタイルを引っ張ってきた
    
    * feat: カードを実装
    
    * feat: cardクラスの追加とボーダーを付けた
    
    * feat: ul>liにした。
    
    * feat: 5件分繰り返し表示するように修正
    
    * feat: タスクタイトルやコンテント、コメントを修正して後続実装者が判断しやすいようにした。
    
    * feat: .badgeの中身を完了にし、スタイルを一部修正
    
    * feat: バッジをよりデザインに寄せた
    
    * feat: gapの調整
    
    * feat: タスクステータスをenumを用いたパーシャル化した
    
    * feat: タスクカードをパーシャル化
    
    * feat: タスクカードの編集ボタンにツールチップを追加し、コードを整理
    
    * feat: セマンティックhtmlに合わせて修正
    
    * feat: link_toを用いた導線を確保
    
    * feat: iconをパーシャル化した
    
    * feat: デモの導線を用意
    
    * feat: タスク詳細の骨組みを実装
    
    * feat: アクションボタンを実装
    
    * feat: コメント欄を実装
    
    * feat: footerを実装
    
    * feat: アバターを上に寄せる
    
    * feat: コメントUIを実装
    
    * feat: コメントをpartial化
    
    * feat: コメントを追加
    
    * feat: dividerをなくしてborderで枠線を定義
    
    * feat: コメント送信ボタンを暫定で非活性化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,14 @@ class TasksController < ApplicationController
   end
 
   def show
+    # 画面確認用のダミー表示
+    @task = Struct.new(:title, :content, :status).new(
+      "Hello task",
+      "ここにタスクの詳細が表示されます。\n\nHello task",
+      "in_progress"
+    )
+
+    render :show
   end
 
   def destroy
